e714867581dac5d19335ee8587997f28aa416772
aspose-com-gists <github@aspose.com>
2019-12-27 21:57:50 +0500

    Examples update for Aspose.PUB for .NET 19.12

---
+++ Examples/CSharp/ProgrammingWithDocuments/EditMetaData.cs
@@ -0,0 +1,35 @@
+﻿using Aspose.Pub;
+using Aspose.Pub.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.ProgrammingWithDocuments
+{
+    class EditMetaData
+    {
+        public static void Run()
+        {
+            //ExStart: EditMetaData
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            string pubFile = dataDir + "document.pub";
+
+            IPubParser parser = PubFactory.CreateParser(pubFile);
+            Document document = parser.Parse();
+
+            document.DocumentSummaryInfo.SetCategory("category");
+            document.DocumentSummaryInfo.SetCompany("company");
+            document.DocumentSummaryInfo.SetLanguage("language");
+
+            document.SummaryInfo.SetComments("comments");
+            document.SummaryInfo.SetKeywords("keywords");
+            document.SummaryInfo.SetLastAuthor("last author");
+            document.SummaryInfo.SetTitle("title");
+            document.SummaryInfo.SetSubject("subject");
+            //ExEnd: EditMetaData
+        }
+    }
+}
